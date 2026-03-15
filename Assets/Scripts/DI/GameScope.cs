cb9246adc2bd87110035fdb0700818326f854dda
Maxim Kiryakov <71668634+maxbritva@users.noreply.github.com>
2026-03-15 20:31:18 +0300

    setup v container

---
+++ Assets/Scripts/DI/GameScope.cs
@@ -0,0 +1,13 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace DI
+{
+    public class GameScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/DI/GlobalScope.cs
@@ -0,0 +1,13 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace DI
+{
+    public class GlobalScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            
+        }
+    }
+}

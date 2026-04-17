2a950507013055942810da21e9bd38940d978e40
Dormitory <g1232234@gmail.com>
2026-04-17 10:49:41 +0300

    Магазин блоков: кнопки вместо слайдера

--- Assets/Scripts/UI/Shop/SellBlocksShop/BuyShopEntry.cs
+++ Assets/Scripts/UI/Shop/SellBlocksShop/BuyShopEntry.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,17 +13,13 @@ public class BuyShopEntry : MonoBehaviour
     public Image resourceIcon;
     public TextMeshProUGUI priceText;
     public Button buyButton;
-    public Slider chooseQuantitySlider;
+    public List<QuantityIncrementButton> quantityIncrementButtons;
 
     private int _chosenQuantity;
     
     private int _price;
     private int _maxCanAfford;
 
-    private void Awake()
-    {
-        chooseQuantitySlider.onValueChanged.AddListener(OnQuantityChange);
-    }
 
     public void Initialize(int entryId, Sprite icon, string resourceName, int price)
     {
@@ -30,35 +27,53 @@ public class BuyShopEntry : MonoBehaviour
         resourceText.text = $"{resourceName}";
         _price = price;
         buyButton.onClick.AddListener(() => Buy?.Invoke(entryId, _chosenQuantity));
+        
+        foreach (var button in quantityIncrementButtons)
+        {
+            button.button.onClick.AddListener(() => OnQuantityChange(button.increment));
+        }
     }
 
     public void Refresh(int maxCanBuy)
     {
         _maxCanAfford =  maxCanBuy;
-        
-        chooseQuantitySlider.interactable = maxCanBuy > 1;
-        
         buyButton.interactable = maxCanBuy != 0;
-
         _chosenQuantity = maxCanBuy == 0 ? 0 : 1;
-
-        chooseQuantitySlider.value = 0;
-
-        if (_chosenQuantity > maxCanBuy)
-        {
-            chooseQuantitySlider.value = 1;
-            _chosenQuantity = maxCanBuy;
-        }
         
+        if (_chosenQuantity > maxCanBuy)
+            _chosenQuantity = maxCanBuy;
+        
+        RefreshButtons();
         countText.text = $"x{_chosenQuantity}";
         priceText.text = $"{Mathf.Max(_chosenQuantity * _price, _price)}";
     }
 
-    private void OnQuantityChange(float sliderValue)
+    private void OnQuantityChange(int increment)
     {
-        var chosenQuantity = 1 + Mathf.RoundToInt(sliderValue * (_maxCanAfford - 1));
+        var chosenQuantity = _chosenQuantity + increment;
         _chosenQuantity = Mathf.Clamp(chosenQuantity, 1, _maxCanAfford);
-        countText.text = $"x{chosenQuantity}";
-        priceText.text = $"{chosenQuantity * _price}";
+        countText.text = $"x{_chosenQuantity}";
+        priceText.text = $"{_chosenQuantity * _price}";
+        RefreshButtons();
     }
+
+    private void RefreshButtons()
+    {
+        foreach (var button in quantityIncrementButtons)
+        {
+            if (button.increment < 0 && _chosenQuantity <= 1)
+                button.button.interactable = false;
+            else if (button.increment > 0 && _chosenQuantity >= _maxCanAfford)
+                button.button.interactable = false;
+            else
+                button.button.interactable = true;
+        }
+    }
+}
+
+[Serializable]
+public class QuantityIncrementButton
+{
+    public Button button;
+    public int increment;
 }
